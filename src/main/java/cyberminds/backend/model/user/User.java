8e4ff8a82c99fcd0f8f3c381054850b59e1f4650
theRealNonso <okoroaforkelechi123@gmail.com>
2023-10-06 16:18:02 +0000

    fixed user model, repository and test

--- src/main/java/cyberminds/backend/model/user/User.java
+++ src/main/java/cyberminds/backend/model/user/User.java
@@ -3,17 +3,19 @@ package cyberminds.backend.model.user;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Document(collection = "users")
 public class User {
 
+    @Id
     private String id;
-
-    private String username;
 
     private String displayName;
 
@@ -22,4 +24,6 @@ public class User {
     private UserStatus status;
 
     private LocalDateTime lastActive;
+
+    private Boolean isVerified;
 }
--- src/main/java/cyberminds/backend/repository/user/UserRepository.java
+++ src/main/java/cyberminds/backend/repository/user/UserRepository.java
@@ -1,4 +1,10 @@
 package cyberminds.backend.repository.user;
 
-public class UserRepository {
+import cyberminds.backend.model.user.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+    User findUserById(String id);
 }
