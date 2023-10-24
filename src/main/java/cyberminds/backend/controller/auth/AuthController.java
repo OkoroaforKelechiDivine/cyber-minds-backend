59bf016806b7e137538b567f12fe9116d02d6d28
theRealNonso <okoroaforkelechi123@gmail.com>
2023-10-24 16:00:53 +0000

    removed cross origin

--- src/main/java/cyberminds/backend/controller/auth/AuthController.java
+++ src/main/java/cyberminds/backend/controller/auth/AuthController.java
@@ -19,7 +19,6 @@ import java.time.LocalDateTime;
 @RestController
 @RequestMapping("/api/auths")
 @Slf4j
-@CrossOrigin(origins = "true", allowCredentials = "true")
 public class AuthController {
 
     @Autowired
